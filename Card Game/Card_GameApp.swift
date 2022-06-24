57311b9ecd6c98bac396d587c7637c78139b939a
mureyvenom <holuwamurewa@gmail.com>
2022-06-24 19:13:27 +0100

    Initial Commit

---
+++ Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by Oluwamurewa Alao on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Card Game
+//
+//  Created by Oluwamurewa Alao on 24/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
